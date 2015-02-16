cb7c13bc637d7f81339b4a6c2afe0cbcbd17f8e6
Collin Mullis <cmullis@saintjoe.edu>
2015-02-16 12:15:24 -0500

    Added an array of fruit in JSON

---
+++ fruit.js
@@ -0,0 +1,12 @@
+/* Code by Collin Mullis
+	CMP237 
+	Ryan Postma's Practicums
+	February 16th 2015
+*/
+[
+{"name": "Pear", "color": "green", "general shape": "sphere"}
+{"name": "Apple", "color": "red", "general shape": "sphere"}
+{"name": "Orange", "color": "orange", "general shape": "sphere"}
+{"name": "Banana", "color": "yellow", "general shape": "cylinder"}
+{"name": "Tomatoe": "color": "red", "general shape": "sphere"}
+]
